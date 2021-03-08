e85223101a0052f250cd1c335277d03bd4a8102d
xymy <thyfan@163.com>
2021-03-08 15:48:11 +0800

    black core

--- swagger_server/core/util.py
+++ swagger_server/core/util.py
@@ -7,12 +7,12 @@ from lxml import etree
 def make_response_json(obj, status=200, headers=None):
     response = json.dumps(obj)
     return current_app.response_class(
-        response, status, headers, mimetype='application/json'
+        response, status, headers, mimetype="application/json"
     )
 
 
 def make_response_xml(element_or_tree, status=200, headers=None):
-    response = etree.tostring(element_or_tree, encoding='utf-8', pretty_print=True)
+    response = etree.tostring(element_or_tree, encoding="utf-8", pretty_print=True)
     return current_app.response_class(
-        response, status, headers, mimetype='application/xml'
+        response, status, headers, mimetype="application/xml"
     )
