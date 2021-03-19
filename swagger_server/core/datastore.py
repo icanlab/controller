f538842f5556d542c4984e227f2587796f84b6ac
xymy <thyfan@163.com>
2021-03-19 16:50:07 +0800

    Add get/set config

--- swagger_server/core/datastore.py
+++ swagger_server/core/datastore.py
@@ -1,4 +1,5 @@
 from lxml import etree
+from ncclient.xml_ import to_ele, to_xml
 from redis import Redis
 
 
@@ -6,19 +7,35 @@ def _ckey(neid, source):
     return "controller:{}:{}".format(neid, source)
 
 
+def _dkey(neid, source):
+    return "device:{}:{}".format(neid, source)
+
+
 class Datastore(object):
     def __init__(self, **kwargs):
         self._redis = Redis(**kwargs)
-        self._parser = etree.XMLParser(remove_blank_text=True)
 
-    def from_xml(self, xml):
-        return etree.fromstring(xml, self.parser)
+    def _get_config(self, key, xpath, ns_map):
+        xml = self._redis.get(key)
+        ele = to_ele(xml)
+        return ele.xpath(xpath, namespaces=ns_map)
 
-    def to_xml(self, ele):
-        return etree.tostring(ele, encoding="utf-8")
-
-    def get_controller_config(self, xpath, ns_map, neid, source):
+    def get_controller_config(self, neid, source, xpath, ns_map):
         key = _ckey(neid, source)
-        data = self._redis.get(key)
-        config = self.from_xml(data)
-        return config.xpath(xpath, namespaces=ns_map)
+        return self._get_config(key, xpath, ns_map)
+
+    def get_device_config(self, neid, source, xpath, ns_map):
+        key = _dkey(neid, source)
+        return self._get_config(key, xpath, ns_map)
+
+    def _set_config(self, key, ele):
+        xml = to_xml(ele)
+        return self._redis.set(key, xml)
+
+    def set_controller_config(self, neid, source, ele):
+        key = _ckey(neid, source)
+        return self._set_config(key, ele)
+
+    def set_device_config(self, neid, source, ele):
+        key = _dkey(neid, source)
+        return self._set_config(key, ele)
