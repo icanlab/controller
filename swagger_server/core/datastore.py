a4b308c5ae3e1c0889e132c8544cbe3442c52c2b
xymy <thyfan@163.com>
2021-03-22 14:04:20 +0800

    Redesign

--- swagger_server/core/datastore.py
+++ swagger_server/core/datastore.py
@@ -15,18 +15,25 @@ class Datastore(object):
     def __init__(self, **kwargs):
         self._redis = Redis(**kwargs)
 
-    def _get_config(self, key, xpath, ns_map):
+    # ==========
+    # GET
+    # ==========
+
+    def _get_config(self, key):
         xml = self._redis.get(key)
-        ele = to_ele(xml)
-        return ele.xpath(xpath, namespaces=ns_map)
+        return to_ele(xml)
 
-    def get_controller_config(self, neid, source, xpath, ns_map):
+    def get_controller_config(self, neid, source):
         key = _ckey(neid, source)
-        return self._get_config(key, xpath, ns_map)
+        return self._get_config(key)
 
-    def get_device_config(self, neid, source, xpath, ns_map):
+    def get_device_config(self, neid, source):
         key = _dkey(neid, source)
-        return self._get_config(key, xpath, ns_map)
+        return self._get_config(key)
+
+    # ==========
+    # SET
+    # ==========
 
     def _set_config(self, key, ele):
         xml = to_xml(ele)
@@ -39,3 +46,20 @@ class Datastore(object):
     def set_device_config(self, neid, source, ele):
         key = _dkey(neid, source)
         return self._set_config(key, ele)
+
+    # ==========
+    # QUERY
+    # ==========
+
+    def _query_config(self, key, xpath, ns_map):
+        xml = self._redis.get(key)
+        ele = to_ele(xml)
+        return ele.xpath(xpath, namespaces=ns_map)
+
+    def query_controller_config(self, neid, source, xpath, ns_map):
+        key = _ckey(neid, source)
+        return self._query_config(key, xpath, ns_map)
+
+    def query_device_config(self, neid, source, xpath, ns_map):
+        key = _dkey(neid, source)
+        return self._query_config(key, xpath, ns_map)
