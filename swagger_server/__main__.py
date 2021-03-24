729dd07fe26286c76e2f02488767b2037fd73db5
xymy <thyfan@163.com>
2021-03-24 21:04:46 +0800

    Add logger

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -3,9 +3,11 @@
 import connexion
 
 from swagger_server import encoder
+from swagger_server.core import init_logger
 
 
 def main():
+    init_logger()
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Mediator controller'}, pythonic_params=True)
